caeaf09e37a831c53bdab996a9872c474bcfb99b
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2025-03-27 23:59:51 +0800

    implemented server-side form validation using  useActionState

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,30 +5,55 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer',
+    }),
+    amount: z.coerce.number().gt(0, 'Amount must be greater than $0'),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.date(),
 });
 
 // Create Invoice
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export async function createInvoice(prevState: State, formData: FormData) {
     // const sampleOfEntries = Object.fromEntries(formData.entries());
     // const { customerId, amount, status } = CreateInvoice.parse(
     //     Object.fromEntries(formData.entries())
     // );
 
-    const { customerId, amount, status } = CreateInvoice.parse({
+    // Validate form using Zod
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Failed to Create Invoice.',
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -38,7 +63,9 @@ export async function createInvoice(formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
             `;
     } catch (error) {
-        console.error(error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
     }
 
     revalidatePath('/dashboard/invoices');
